68cc18f4f190975cb7a1411a251c89b8237f1c66
Henrique Kisaki <satoshikisaki@hotmail.com>
2023-10-27 19:04:51 -0300

    Add Item unit tests

--- src/main/java/br/com/cartpurchase/model/Item.java
+++ src/main/java/br/com/cartpurchase/model/Item.java
@@ -193,37 +193,18 @@ public class Item {
   }
 
   @Override
-  public int hashCode() {
-    return Objects.hash(id, productTitle, description, brandName, material, color, quantity, dimensions, price);
-  }
-
-  @Override
   public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class Item {\n");
-    
-    sb.append("    id: ").append(toIndentedString(id)).append("\n");
-    sb.append("    productTitle: ").append(toIndentedString(productTitle)).append("\n");
-    sb.append("    description: ").append(toIndentedString(description)).append("\n");
-    sb.append("    brandName: ").append(toIndentedString(brandName)).append("\n");
-    sb.append("    material: ").append(toIndentedString(material)).append("\n");
-    sb.append("    color: ").append(toIndentedString(color)).append("\n");
-    sb.append("    quantity: ").append(toIndentedString(quantity)).append("\n");
-    sb.append("    dimensions: ").append(toIndentedString(dimensions)).append("\n");
-    sb.append("    price: ").append(toIndentedString(price)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
-    }
-    return o.toString().replace("\n", "\n    ");
+    return "Item{" +
+            "id=" + id +
+            ", productTitle='" + productTitle + '\'' +
+            ", description='" + description + '\'' +
+            ", brandName='" + brandName + '\'' +
+            ", material='" + material + '\'' +
+            ", color='" + color + '\'' +
+            ", quantity=" + quantity +
+            ", dimensions=" + dimensions +
+            ", price=" + price +
+            '}';
   }
 
 }
